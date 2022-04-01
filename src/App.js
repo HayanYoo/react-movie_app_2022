ff4c0700e748e914b59904e5b5667c14f5f7f43e
MILL_WHITE <hayanyoo.dev@gmail.com>
2022-04-01 13:26:54 +0900

    function에서 class로 변경하기

--- src/App.js
+++ src/App.js
@@ -1,55 +1,29 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-function Food({name, picture, rating}) {
-    return <div>
-        <h1>I like {name}</h1>
-        <h4>{rating}/5.0</h4>
-        <img src={picture} alt={name}/>
-    </div>
+class App extends React.Component{
+    state = {
+        count : 0
+    };
+
+    add = () => {
+        this.setState(current =>( {count : current.count + 1}));
+    };
+    minus = () => {
+        this.setState(current =>( {count : current.count - 1}));
+    };
+
+    render() {
+        return (
+            <div>
+                <div>The number is {this.state.count}</div>
+                <button onClick={this.add}>Add</button>
+                <button onClick={this.minus}>Minus</button>
+            </div>
+        );
+    }
+
 }
 
-const foodILike = [
-    {
-        id: 1,
-        name: "Kimchi",
-        image: "https://cdn.imweb.me/thumbnail/20200415/6b6e035658bac.png",
-        rating : 5
-
-    },
-    {
-        id: 2,
-        name: "삼겹살",
-        image: "https://pds.joongang.co.kr/news/component/htmlphoto_mmdata/201702/27/117f5b49-1d09-4550-8ab7-87c0d82614de.jpg",
-        rating: 4.9
-
-    },
-    {
-        id: 3,
-        name: "비빔밥",
-        image: "https://recipe1.ezmember.co.kr/cache/recipe/2016/04/20/572d121195216a73d6870429148cd8fd1.jpg",
-        rating: 4.8
-
-    },
-]
-
-Food.propTypes = {
-    name : PropTypes.string.isRequired,
-    picture : PropTypes.string.isRequired,
-    rating : PropTypes.number.isRequired
-};
-
-
-function renderFood(dish) {
-    return <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating}/>
-}
-
-function App() {
-    return (
-        <div>
-            {foodILike.map(renderFood)}
-        </div>
-    );
-}
 
 export default App;
