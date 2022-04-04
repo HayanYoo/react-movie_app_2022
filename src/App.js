876dcc05b105838d29097402a38b2646cdfa4c70
MILL_WHITE <hayanyoo.dev@gmail.com>
2022-04-04 12:42:11 +0900

    feat todo list 만들기

--- src/App.js
+++ src/App.js
@@ -1,54 +1,10 @@
-import React from "react";
+import React, {useState} from "react";
+import Todo from "./Todo.js";
 
-class App extends React.Component{
-    state = {
-        count : 0,
-        minutes : 0, 
-    };
-
-    add = () => {
-        this.setState(current =>( {count : current.count + 1}));
-    };
-    minus = () => {
-        this.setState(current =>( {count : current.count - 1}));
-    };
-
-    onChange = (event) => {
-            this.setState({minutes : event.target.value})
-    };
-
-    render() {
-        return (
-            <div>
-                <div>The number is {this.state.count}</div>
-                <button onClick={this.add}>Add</button>
-                <button onClick={this.minus}>Minus</button>
-                <hr></hr>
-
-                <h1 className="converter">Super Converter</h1>
-
-                <label htmlFor="minutes">Minutes</label>
-                <input 
-                value={this.state.minutes} 
-                id="minutes" 
-                placeholder="Minutes" 
-                type="number" 
-                onChange ={this.onChange} 
-                />
-                <h4>You want to convert {this.state.minutes}</h4>
-              
-                <label htmlFor="hours">Minutes</label>
-                <input
-                value={this.state.minutes/60} 
-                id="hours" 
-                placeholder="Hours" 
-                type="number"
-                />
-
-            </div>
-        );
-    }
-
+function App() {
+    return(
+    <Todo/>
+)
 }
 
 
