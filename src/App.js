c1021b9f3db400670dad50d66f50b6546310c229
MILL_WHITE <hayanyoo.dev@gmail.com>
2022-04-04 13:38:19 +0900

    영화 리스트 가져오기

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import Todo from "./Todo.js";
 import Coin from "./Coin.js";
 
 function App() {
+    const [loading, setLoading] = useState(true);
+    const [movies, setMovies] = useState([]);
+    const getMovies = async () => {
+        const json = await (
+            await fetch(
+                `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
+            )
+        ).json();
+        setMovies(json.data.movies);
+        setLoading(false);
+    }
+    useEffect(() => {
+        getMovies();
+
+    }, [])
+    console.log(movies)
     return (
         <div>
-            <Todo/>
-            <Coin/>
+            {loading ?
+                <h1>Loading...</h1> :
+                movies.map(movie =>
+                    <div key={movie.id}>
+                        <img src={movie.medium_cover_image}/>
+                        <h2>{movie.title}</h2>
+                        <p>{movie.summary}</p>
+                        <ul>
+                            {movie.genres.map( (g, index) => (
+                                <li key={index}>{g}</li>
+                            ))}
+                        </ul>
+                    </div>
+                )}
         </div>
     )
 }
