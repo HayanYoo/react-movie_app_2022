8e78200f1639d777e95f3203e127c09851d688ac
MILL_WHITE <hayanyoo.dev@gmail.com>
2022-04-13 17:59:18 +0900

    feat graphql로 데이터 받아와서 Link를 이용해 routing 하기

--- src/graphql/routes/Home.js
+++ src/graphql/routes/Home.js
@@ -1,3 +1,23 @@
 import React from "react";
+import { gql } from "apollo-boost"
+import {useQuery} from "@apollo/client";
+import Movie from "../components/Movie";
 
-export default () => <h1>Home</h1>
+const GET_MOVIES = gql`{
+    movies {
+        id
+        description_intro
+        title
+    }
+}`
+
+
+export default () => {
+    const {loading, data} = useQuery(GET_MOVIES)
+    return (
+        <div>
+            {loading ? "Loading..." : null}
+            {!loading && data.movies && data.movies.map(m => <Movie key={m.id} id={m.id}/>)}
+        </div>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import App from './App';
 import * as ReactDOMClient from 'react-dom/client';
+import {ApolloProvider} from "@apollo/react-hooks";
+import client from "./graphql/apollo";
 
-ReactDOMClient.createRoot(document.getElementById('root')).render(<App />);
+
+ReactDOMClient
+    .createRoot(document.getElementById('root'))
+    .render(
+        <ApolloProvider client={client}>
+        <App />
+        </ApolloProvider>
+    );
 
