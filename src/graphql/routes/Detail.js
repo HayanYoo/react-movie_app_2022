f3b1d3ddda72fd30a16515bff8696e453360400f
MILL_WHITE <hayanyoo.dev@gmail.com>
2022-04-14 16:46:32 +0900

    feat id로 detail 정보 받아오기

--- src/graphql/routes/Detail.js
+++ src/graphql/routes/Detail.js
@@ -1,3 +1,29 @@
 import React from "react";
+import {gql} from "apollo-boost";
+import {useParams} from "react-router-dom";
+import {useQuery} from "@apollo/client";
 
-export default () => <h1>Detail</h1>
+
+const GET_MOVIE = gql`
+    query getMovie($id: Int!) {
+        movie(id : $id){
+            id
+            title
+            medium_cover_image
+            description_intro
+        }
+    }`;
+
+export default () => {
+    const {id} = useParams();
+    const {loading, data} = useQuery(GET_MOVIE, {
+        variables: {id : +id}
+    });
+    if(loading){
+        return "loading"
+    }
+    if (data && data.movie) {
+        return data.movie.title;
+    }
+    return <h1>Detail</h1>
+}
